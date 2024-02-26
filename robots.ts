c652113db685ee0b19b419cc15c81d508aa42771
Ryan Oliver <ryanandrewoliver@gmail.com>
2024-02-26 09:57:09 +0200

    sitemap and robots url

--- robots.ts
+++ robots.ts
@@ -6,6 +6,6 @@ export default function robots(): MetadataRoute.Robots {
       userAgent: "*",
       allow: "/",
     },
-    sitemap: "https://konten.dev/sitemap.xml",
+    sitemap: "https://www.konten.dev/sitemap.xml",
   };
 }
--- sitemap.ts
+++ sitemap.ts
@@ -3,7 +3,7 @@ import { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://konten.dev",
+      url: "https://www.konten.dev",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1,
