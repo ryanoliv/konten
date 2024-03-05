8af66548674facf24851b15da48cede640ac3a5a
Ryan Oliver <ryanandrewoliver@gmail.com>
2024-03-05 14:39:49 +0200

    page transitions and blog mobile adjustments

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import "@/styles/globals.css";
 import { useEffect, useState } from "react";
 import Loader from "@/components/Loader/Loader";
@@ -14,7 +15,26 @@ const dmSans = DM_Sans({ subsets: ["latin"], weight: ["400", "500", "700"] });
 const Footer = dynamic(() => import("@/components/Footer/Footer"));
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [pageLoading, setPageLoading] = useState(false);
+
   const [locomotiveScroll, setLocomotiveScroll] = useState<any | null>(null);
+
+  // Detecting route changes
+  useEffect(() => {
+    const handleStart = () => setPageLoading(true);
+    const handleComplete = () => setPageLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete); // consider adding this to handle load errors
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  }, [router]);
 
   useEffect(() => {
     import("locomotive-scroll").then((LocomotiveScrollModule) => {
@@ -41,16 +61,24 @@ export default function App({ Component, pageProps }: AppProps) {
     <LocomotiveScrollProvider>
       <div className={dmSans.className}>
         <Loader />
-        <HeroBackgroundImg />
-        <header className={"fixed top-0 w-full z-20"}>
-          <Nav />
-        </header>
-        <main className="content" data-scroll-container>
-          <Component {...pageProps} />
-        </main>
-        <footer>
-          <Footer />
-        </footer>
+        <div
+          className={`page-transition ${
+            pageLoading
+              ? "page-transition-enter"
+              : "page-transition-enter-active"
+          }`}
+        >
+          <HeroBackgroundImg />
+          <header className={"fixed top-0 w-full z-20"}>
+            <Nav />
+          </header>
+          <main className="content" data-scroll-container>
+            <Component {...pageProps} />
+          </main>
+          <footer>
+            <Footer />
+          </footer>
+        </div>
       </div>
     </LocomotiveScrollProvider>
   );
