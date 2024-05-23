4942bf3e4ccab0e58d3e4315bb34be6bf9158222
Ryan Oliver <ryanandrewoliver@gmail.com>
2024-05-23 09:01:02 +0200

    copywrite change back

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -75,8 +75,7 @@ export default function Footer() {
         </div>
       </div>
       <div className={styles.footerBottom}>
-        {/* <p>&copy; Konten 2024. All Rights Reserved.</p> */}
-        <p>Designed & Developed by Ryan Oliver.</p>
+        <p>&copy; Konten 2024. All Rights Reserved.</p>
         <Link href="/privacy-policy">Privacy Policy</Link>
         <Link href="/terms-of-service">Terms of Service</Link>
       </div>
